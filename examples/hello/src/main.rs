d80f1da56c3372f6e4c422d5657e307729977438
gretor0231 <gretor0231@gmail.com>
2024-07-18 18:31:42 +0800

    main: 1. added all main rs files.

---
+++ examples/hello/src/main.rs
@@ -0,0 +1,15 @@
+use hello::{english, russian, spanish};
+
+fn main() {
+    // hello::greet_a_variables
+    let missiles = 8;
+    let ready =2;
+
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    println!("{} missiles left", missiles - ready);
+
+
+
+
+
+}
